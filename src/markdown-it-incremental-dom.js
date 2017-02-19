e4848a7a06a62fa526bd420fb6c0243e45dd10f7
Yuki Hattori <yukihattori1116@gmail.com>
2017-02-20 03:20:39 +0900

    Remove expart usage
    The code of expert usage seems better but actually it has little
    meaning. Some plugins have close relation between injected tokens
    and rendering rules.

--- src/markdown-it-incremental-dom.js
+++ src/markdown-it-incremental-dom.js
@@ -4,7 +4,11 @@ import rendererMixin from './mixins/renderer'
 const mixinTo = (base, mixin) =>
   (Object.assign(Object.create(Object.getPrototypeOf(base)), base, mixin))
 
-const injectMarkdownItIncrementalDOM = (md, incrementalDOM) => {
+const processIncrementalDOMArgument = incrementalDOM =>
+  ((!incrementalDOM && typeof window !== 'undefined') ? window.IncrementalDOM : incrementalDOM)
+
+export default function(md, target) {
+  const incrementalDOM = processIncrementalDOMArgument(target)
   const mixin = rendererMixin(incrementalDOM)
 
   md.renderToIncrementalDOM = function (src, env = {}) {
@@ -14,18 +18,3 @@ const injectMarkdownItIncrementalDOM = (md, incrementalDOM) => {
     return mixinTo(this.renderer, mixin).render(this.parseInline(src, env), this.options, env)
   }
 }
-
-const processIncrementalDOMArgument = incrementalDOM =>
-  ((!incrementalDOM && typeof window !== 'undefined') ? window.IncrementalDOM : incrementalDOM)
-
-const markdownitIncrementalDOM = function (...args) {
-  // new markdownitIncrementalDOM(baseIncrementalDOM)
-  if (this.constructor === markdownitIncrementalDOM) {
-    return mixinTo(new Renderer(), rendererMixin(processIncrementalDOMArgument(args[0])))
-  }
-
-  // MarkdownIt().use(markdownitIncrementalDOM, baseIncrementalDOM)
-  return injectMarkdownItIncrementalDOM(args[0], processIncrementalDOMArgument(args[1]))
-}
-
-export { markdownitIncrementalDOM as default }
