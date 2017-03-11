9f03342dd0852feb209b2de456f2170777385fbb
Yuki Hattori <yukihattori1116@gmail.com>
2017-03-11 18:04:13 +0900

    Add test of the initialize with option

--- test/markdown-it-incremental-dom.js
+++ test/markdown-it-incremental-dom.js
@@ -1,10 +1,12 @@
 import assert from 'assert'
-import MarkdownIt from 'markdown-it'
+import sinon from 'sinon'
 import IncrementalDOM from 'incremental-dom'
+import MarkdownIt from 'markdown-it'
+import { Parser } from 'htmlparser2'
 import MarkdownItIncrementalDOM from '../src/markdown-it-incremental-dom'
 
 describe('markdown-it-incremental-dom', () => {
-  const md = () => MarkdownIt().use(MarkdownItIncrementalDOM, IncrementalDOM)
+  const md = (opts = {}) => MarkdownIt().use(MarkdownItIncrementalDOM, IncrementalDOM, opts)
 
   describe('markdownIt().use', () => {
     context('when Incremental DOM argument is omitted', () => {
@@ -17,6 +19,36 @@ describe('markdown-it-incremental-dom', () => {
       it('succeeds injection if window.IncrementalDOM is defined', () => {
         window = { IncrementalDOM }
         assert.doesNotThrow(() => MarkdownIt().use(MarkdownItIncrementalDOM))
+      })
+    })
+
+    context('with option', () => {
+      describe('incrementalizeDefaultRules property', () => {
+        let spy
+
+        beforeEach(() => { spy = sinon.spy(Parser.prototype, 'write') })
+        afterEach(() => spy.restore())
+
+        const mdString = '`code_inline`'
+        const expectedHTML = '<code>code_inline</code>'
+
+        context('when it is false', () => {
+          it('parses HTML string by htmlparser2', () => {
+            const func = md({ incrementalizeDefaultRules: false }).renderInlineToIncrementalDOM(mdString)
+
+            IncrementalDOM.patch(document.body, func)
+            assert(spy.calledWith(expectedHTML))
+          })
+        })
+
+        context('when it is true', () => {
+          it('does not parse HTML string in overridden rule', () => {
+            const func = md({ incrementalizeDefaultRules: true }).renderInlineToIncrementalDOM(mdString)
+
+            IncrementalDOM.patch(document.body, func)
+            assert(!spy.calledWith(expectedHTML))
+          })
+        })
       })
     })
   })
