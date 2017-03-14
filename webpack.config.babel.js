499a5cb1228966261fcff07bcbfc2ecf04c2235c
Yuki Hattori <yukihattori1116@gmail.com>
2017-03-14 21:29:48 +0900

    Improve JS packaging for browser
    - Add banner comment to show included licenses
    - Generate uncompressed JS

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,15 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path'
+import webpack from 'webpack'
 import packageConfig from './package.json'
 
+const banner = `${packageConfig.name} ${packageConfig.version}
+${packageConfig.repository.url}
+
+Includes htmlparser2
+https://github.com/fb55/htmlparser2/
+https://github.com/fb55/htmlparser2/raw/master/LICENSE
+
+@license ${packageConfig.license}
+${packageConfig.repository.url}/raw/master/LICENSE`
+
+const basename = path.basename(packageConfig.browser, '.js')
+
 export default {
-  entry: './entry.js',
+  entry: {
+    [basename]: './entry.js',
+    [`${basename}.min`]: './entry.js',
+  },
   output: {
     path: path.resolve(__dirname, path.dirname(packageConfig.browser)),
-    filename: path.basename(packageConfig.browser),
+    filename: '[name].js',
     libraryTarget: 'umd',
     umdNamedDefine: true,
   },
+  plugins: [
+    new webpack.BannerPlugin(banner),
+    new webpack.optimize.UglifyJsPlugin({ include: /\.min\.js($|\?)/i }),
+  ],
   module: {
     rules: [
       {
