ffd6cabc446b9f9edcc28c0edc2402328a8aeb2d
Yuki Hattori <yukihattori1116@gmail.com>
2017-12-30 15:55:33 +0900

    Add import babel-polyfill to entry.js (for browser)

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
 /* eslint import/prefer-default-export: 0 */
+import 'babel-polyfill'
 import markdownitIncrementalDOM from './src/markdown-it-incremental-dom'
 
 export { markdownitIncrementalDOM }
