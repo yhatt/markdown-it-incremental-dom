7efc71319b0d22b2a30a174d3cfa7d984200105f
Yuki Hattori <yukihattori1116@gmail.com>
2017-12-30 15:56:23 +0900

    Fix to use object rest spread syntax

--- src/markdown-it-incremental-dom.js
+++ src/markdown-it-incremental-dom.js
@@ -2,13 +2,13 @@ import rendererMixin from './mixins/renderer'
 import incrementalizedRules from './mixins/rules'
 
 const mixinTo = (base, mixin) =>
-  (Object.assign(Object.create(Object.getPrototypeOf(base)), base, mixin))
+  Object.assign(Object.create(Object.getPrototypeOf(base)), base, mixin)
 
 const processIncrementalDOMArgument = incrementalDOM =>
-  ((!incrementalDOM && typeof window !== 'undefined') ? window.IncrementalDOM : incrementalDOM)
+  (!incrementalDOM && typeof window !== 'undefined') ? window.IncrementalDOM : incrementalDOM
 
 export default function (md, target, opts = {}) {
-  const options = Object.assign({ incrementalizeDefaultRules: true }, opts)
+  const options = { incrementalizeDefaultRules: true, ...opts }
   const incrementalDOM = processIncrementalDOMArgument(target)
   const mixin = rendererMixin(incrementalDOM)
 
@@ -16,8 +16,7 @@ export default function (md, target, opts = {}) {
     const renderer = mixinTo(this.renderer, mixin)
 
     if (options.incrementalizeDefaultRules) {
-      const overridedRules = Object.assign({}, renderer.rules, incrementalizedRules(incrementalDOM))
-      renderer.rules = overridedRules
+      renderer.rules = { ...renderer.rules, ...incrementalizedRules(incrementalDOM) }
     }
 
     return renderer.render(this[method](src, env), this.options, env)
