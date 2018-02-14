d19396b71d6ed82df1e76ed78839e09298316968
Yuki Hattori <yukihattori1116@gmail.com>
2018-02-14 21:06:34 +0900

    Define IncrementalDOMRenderer as public getter (for expert)

--- src/markdown-it-incremental-dom.js
+++ src/markdown-it-incremental-dom.js
@@ -6,27 +6,34 @@ export default function(md, target, opts = {}) {
   const incrementalDOM = !target && window ? window.IncrementalDOM : target
   const mixin = renderer(incrementalDOM)
 
-  const render = function(method, src, env) {
-    const extendedRenderer = Object.assign(
-      Object.create(Object.getPrototypeOf(this.renderer)),
-      this.renderer,
-      mixin
-    )
+  Object.defineProperty(md, 'IncrementalDOMRenderer', {
+    get() {
+      const extended = Object.assign(
+        Object.create(Object.getPrototypeOf(this.renderer)),
+        this.renderer,
+        mixin
+      )
 
-    if (options.incrementalizeDefaultRules) {
-      extendedRenderer.rules = {
-        ...extendedRenderer.rules,
-        ...rules(incrementalDOM),
+      if (options.incrementalizeDefaultRules) {
+        extended.rules = { ...extended.rules, ...rules(incrementalDOM) }
       }
-    }
-
-    return extendedRenderer.render(this[method](src, env), this.options, env)
-  }
+      return extended
+    },
+  })
 
   md.renderToIncrementalDOM = function(src, env = {}) {
-    return render.call(this, 'parse', src, env)
+    return this.IncrementalDOMRenderer.render(
+      this.parse(src, env),
+      this.options,
+      env
+    )
   }
+
   md.renderInlineToIncrementalDOM = function(src, env = {}) {
-    return render.call(this, 'parseInline', src, env)
+    return this.IncrementalDOMRenderer.render(
+      this.parseInline(src, env),
+      this.options,
+      env
+    )
   }
 }
